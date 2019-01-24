567a16e37a25ee0045dcb33e8f5fc6404c21f14d
Adam Jozefiak <adam.jozefiak@alumni.ubc.ca>
2019-01-24 01:34:40 -0800

    changed path for mounting repo

--- run-notebook.sh
+++ run-notebook.sh
@@ -8,4 +8,4 @@ fi
 
 # Otherwise. 
 \cp ./_build/jupyter/$1 ./notebooks/$1 # Move first argument to the right folder. Will overwrite. 
-jupyter nbconvert ./notebooks/$1 --ExecutePreprocessor.timeout=600 --to notebook --inplace --allow-errors --execute  # Runs the file. 
+jupyter nbconvert ./notebooks/$1 --ExecutePreprocessor.timeout=600 --to notebook --inplace  --execute  # Runs the file. 
